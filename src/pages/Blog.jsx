d7e3075323cc80ff2ac3eb9daad5afef11ab9543
Paran Sonthalia <paran.sonthalia@gmail.com>
2021-08-28 18:26:52 -0700

    working blog not sexy

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import BottomNav from "../components/BottomNav";
 import TopNav from "../components/TopNav";
+import BlogComponent from "../components/BlogComponent";
 
 export default function Blog() {
   return (
     <>
       <TopNav blog />
-      Blog
+      <BlogComponent />
       <BottomNav />
     </>
   );
